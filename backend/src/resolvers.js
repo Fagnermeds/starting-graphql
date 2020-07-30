e2e18e5dccd9d98b40f1985e63b3667ed7ebf098
Fagner Medeiros <fagnermeds@gmail.com>
2020-07-30 11:15:02 -0300

    create server.js

---
+++ backend/src/resolvers.js
@@ -0,0 +1,15 @@
+const user = [
+  { id: 1, name: 'Fagner', email: 'fagnermeds@gmail.com' },
+  { id: 2, name: 'Maria', email: 'maria@gmail.com' },
+];
+
+module.exports = {
+  Query: {
+    users: () => users,
+    user: () => users[0],
+  },
+
+  Mutation: {
+    createUser: () => {},
+  }
+};
